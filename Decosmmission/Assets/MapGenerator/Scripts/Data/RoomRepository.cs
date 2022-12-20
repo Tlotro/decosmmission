6029b84755d800c2e8d65d46889bd6382e81e34a
Linux Torbolts <114951816+MakimaGorky@users.noreply.github.com>
2022-12-20 15:15:31 +0300

    Rooms copying added

--- Decosmmission/Assets/MapGenerator/Scripts/Data/RoomRepository.cs
+++ Decosmmission/Assets/MapGenerator/Scripts/Data/RoomRepository.cs
@@ -5,7 +5,7 @@ public class RoomRepository : MonoBehaviour
     [SerializeField] private RoomDesign[] designs;
 
     public int Count => designs.Length;
-    
+
     public Room GetRandomRoom()
     {
         return designs[Random.Range(0, designs.Length)].ToRoom();
