40c5d52d1fb427c0a63daae99f96f15ea72cdbad
Tlotro <misha.pletneow@yandex.ru>
2023-04-02 20:53:23 +0300

    Fix generator not working due to sound bug

--- Decosmmission/Assets/MapGenerator/Scripts/Generator.cs
+++ Decosmmission/Assets/MapGenerator/Scripts/Generator.cs
@@ -12,8 +12,8 @@ public class Generator : MonoBehaviour
 
     private void Start()
     {
-        AudioMaster.instance.Appear("Theme", "Test_Tune", MixerGroup.Music, 0.6f, 0, true, 0.0f);
         Generate((int)((StaticGameData.GlobalInstance["GeneratorValues"] as StaticGameData)["RoomCount"]), (StaticGameData.GlobalInstance["GeneratorValues"] as StaticGameData)["PlayerObject"] as GameObject);
+        AudioMaster.instance.Appear("Theme", "Test_Tune", MixerGroup.Music, 0.6f, 0, true, 0.0f);
     }
 
     public int MapSizex { get; private set; }
